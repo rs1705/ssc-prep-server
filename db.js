edec9107af2d400939746ceae68f90bd75d67423
rs1705 <asmrahul17@gmail.com>
2025-09-25 22:53:11 +0530

    added env file for mongourl

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 async function connectDB() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://asmrahul17:4i50TXE5NJRkRL2x@ssc-prep.bs10gh2.mongodb.net/test?retryWrites=true&w=majority&appName=ssc-prep"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to DB successfully.");
   } catch (error) {
     console.log(error);
